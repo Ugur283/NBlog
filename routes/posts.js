97c821e3fd9e8bf1721634b3cc4dc592e9631def
Uğur ŞENEL <115794109+Ugur283@users.noreply.github.com>
2023-08-14 22:55:43 +0300

    add posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post')
+const path = require('path')
 
 
 router.get('/new' , (req, res) =>{
     res.render('site2/addpost')
 })
 
+router.get('/:id' , (req, res) =>{
+    Post.findById(req.params.id).lean().then(post => {
+        res.render('site2/post' , {post:post})
+    })
+})
+
+
 router.post('/test' , (req, res) =>{
-    Post.create(req.body)
+
+    let post_image = req.files.post_image
+
+    post_image.mv(path.resolve(__dirname, '../public/img/postimages' , post_image.name))
+
+
+    Post.create({
+        ...req.body,
+        post_image:`/public/img/postimages/${post_image.name}`
+    })
+
     res.redirect('/')
 })
 
