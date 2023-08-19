8647d8e60e5685d395b218cf73068d676747b090
Uğur ŞENEL <115794109+Ugur283@users.noreply.github.com>
2023-08-19 20:38:22 +0300

    active contact page and added paginations

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const hostname = "127.0.0.1";
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const {generateDate, limit, truncate} = require("./helpers/hbs")
+const {generateDate, limit, truncate, paginate} = require("./helpers/hbs")
 const expressSession = require("express-session");
 const MongoStore = require("connect-mongo");
 const methodOverride = require('method-override')
@@ -41,7 +41,8 @@ const hbs = exphbs.create({
   helpers: {
     generateDate: generateDate ,
     limit: limit,
-    truncate:truncate 
+    truncate:truncate,
+    paginate:paginate
   }
 })
 
@@ -84,11 +85,13 @@ const main = require("./routes/main");
 const posts = require("./routes/posts");
 const users = require("./routes/users");
 const admin = require('./routes/admin/index')
+const contact = require('./routes/contact')
 
 app.use("/", main);
 app.use("/posts", posts);
 app.use("/users", users);
 app.use("/admin", admin);
+app.use("/contact", contact);
 
 app.listen(port, hostname, () => {
   console.log(`Server Çalışıyor , http://${hostname}:${port}/`);
